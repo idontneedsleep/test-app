a4b600cccc93a8f8d127631d6d092b4d16cd4b42
Евеліна Федоренко <ipz192_feo@student.ztu.edu.ua>
2025-05-24 21:27:47 +0300

    init

---
+++ test-app/resources/js/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+</script>
+
+<template>
+    <form>
+        <div class="mb-3">
+            <label class="form-label">Deal Owner</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Deal Name</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Account Name</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Type</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Expected Revenue</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Lead Source</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Contact Name</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Amount</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Closing Date</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Stage</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Probability (%)</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Next Step</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Campaign Source</label>
+            <input type="text" class="form-control">
+        </div>
+        <div>
+            <button type="submit" class="btn btn-primary mb-3">Save</button>
+        </div>
+    </form>
+</template>
